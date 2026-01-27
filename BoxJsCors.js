38544a9510a4bf8d0494c9e0820c26cec5666f48
QinyRui <eqinivd19@163.com>
2026-01-27 22:55:30 +0800

    更新 BoxJsCors.js

--- BoxJsCors.js
+++ BoxJsCors.js
@@ -1,12 +1,20 @@
-// 跨域重写脚本（scriptingAPP 专用）
-const match = $request.url.indexOf("boxjs.com/api/boxjs/get") > -1;
-if (match) {
+// Scripting APP 专用 BoxJs 全接口跨域重写脚本
+const boxjsUrls = [
+    "boxjs.com/api/boxjs/get",
+    "boxjs.com/api/boxjs/set",
+    "boxjs.com/query/data",
+    "boxjs.com/update/data"
+];
+const isBoxJsRequest = boxjsUrls.some(url => $request.url.indexOf(url) > -1);
+
+if (isBoxJsRequest) {
     const res = {
         headers: {
             ...$response.headers,
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+            "Access-Control-Max-Age": "86400"
         },
         body: $response.body
     };
