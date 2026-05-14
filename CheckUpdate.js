9e62756fcf03810a63226b7271cd25a3d5be5784
QinyRui <eqinivd19@163.com>
2026-05-14 23:47:22 +0800

    删除 CheckUpdate.js

--- CheckUpdate.js
+++
@@ -1,7 +0,0 @@
-const $http = $httpClient;
-const url="https://raw.githubusercontent.com/QinyRui/QYR-/main/MihoyoSign.plugin";
-$http.get({url},(err,resp,data)=>{
-  if(err){console.log("[Mihoyo][Update] 检查失败");$done();return;}
-  console.log("[Mihoyo][Update] 最新插件可用");
-  $done();
-});
